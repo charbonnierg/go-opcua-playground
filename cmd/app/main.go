ab3f617cb396352152a11c5f53d2ded2b837586c
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-04-15 16:10:04 +0200

    added subscription to NATS app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,25 +2,39 @@ package main
 
 import (
 	"context"
-	"log"
+	"os"
+	"os/signal"
 	"playground/pkg/nats"
+
+	"go.uber.org/zap"
 )
 
 // Very minimalist lifecycle of an NATS app in go
 func main() {
+	logger := zap.NewExample()
 	ctx := context.Background()
 	// Create the app
 	app := nats.NewApplication("nats://localhost:4222")
 	// Provision the app
 	if err := app.Provision(ctx); err != nil {
-		log.Fatalf("Failed to provision the app: %v", err)
+		logger.Fatal("Failed to provision the app", zap.Error(err))
 	}
 	// Start the app
 	if err := app.Start(); err != nil {
-		log.Fatalf("Failed to start the app: %v", err)
+		logger.Fatal("Failed to start the app", zap.Error(err))
 	}
-	// Stop the app
-	if err := app.Stop(); err != nil {
-		log.Fatalf("Failed to stop the app: %v", err)
-	}
+	// Stop the app on exit
+	defer func() {
+		if err := app.Stop(); err != nil {
+			logger.Fatal("Failed to stop the app", zap.Error(err))
+		}
+	}()
+	// catch ctrl-c and gracefully shutdown the server.
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+	defer signal.Stop(sigch)
+	logger.Info("Press Ctrl+C to exit")
+
+	<-sigch
+	logger.Info("Shutting down the app...")
 }
--- pkg/nats/app.go
+++ pkg/nats/app.go
@@ -2,9 +2,11 @@ package nats
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/micro"
 	"go.uber.org/zap"
 )
 
@@ -37,21 +39,15 @@ func (a *NatsApplication) Provision(ctx context.Context) error {
 
 // Start starts the application.
 func (a *NatsApplication) Start() error {
-	timeout := a.ConnectionTimeout
-	// Default timeout
-	if timeout == 0 {
-		timeout = 5 * time.Second
-	}
-	// Connect to the NATS server
-	options := nats.GetDefaultOptions()
-	options.Url = a.NatsServerUrl
-	options.Timeout = timeout
 	a.logger.Info("Connecting to NATS server", zap.String("url", a.NatsServerUrl))
-	conn, err := options.Connect()
+	conn, err := a.connect()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to connect to NATS server: %w", err)
 	}
 	a.conn = conn
+	if err := a.subscribe(); err != nil {
+		return fmt.Errorf("failed to subscribe to NATS server: %w", err)
+	}
 	a.logger.Info("Connected to NATS server")
 	return nil
 }
@@ -64,6 +60,49 @@ func (a *NatsApplication) Stop() error {
 	}
 	a.logger.Info("Connection to NATS server closed")
 	return nil
+}
+
+// connect is a private function used to connect the NATS client
+func (a *NatsApplication) connect() (*nats.Conn, error) {
+	timeout := a.ConnectionTimeout
+	// Default timeout
+	if timeout == 0 {
+		timeout = 5 * time.Second
+	}
+	// Connect to the NATS server
+	options := nats.GetDefaultOptions()
+	options.Url = a.NatsServerUrl
+	options.Timeout = timeout
+	return options.Connect()
+}
+
+// subscribe is a private function used to subscribe to NATS
+func (a *NatsApplication) subscribe() error {
+	// Define the callback function
+	cb := func(msg micro.Request) {
+		a.logger.Info("Received a message", zap.String("subject", msg.Subject()), zap.String("data", string(msg.Data())))
+		msg.Respond([]byte("Hello!"))
+	}
+	// Add the service
+	service, err := micro.AddService(a.conn, micro.Config{
+		Name:        "demo",
+		Version:     "0.1.0",
+		Description: "A demo micro-service",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to add service: %w", err)
+	}
+	// Add the endpoint
+	if err := service.AddEndpoint("foo", HandlerFunc(cb)); err != nil {
+		return fmt.Errorf("failed to add endpoint: %w", err)
+	}
+	return nil
+}
+
+type HandlerFunc func(micro.Request)
+
+func (h HandlerFunc) Handle(msg micro.Request) {
+	h(msg)
 }
 
 // Typeguards
